71ffef7feb364284c1f2de44819c589b80378c19
Ajeeta Asthana <ajita.asthana@gmail.com>
2023-12-12 19:33:23 -0500

    swap  function

--- CPP_UG/hands_on.cpp
+++ CPP_UG/hands_on.cpp
@@ -2,8 +2,20 @@
 #include <cstdlib> // C standard library, defines rand(), srand(), RAND_MAX
 #include <ctime>
 using namespace std;
+
+void swap(int& p, int& q)
+{
+    int t;
+    t = p;
+    p = q;
+    q = t;
+}
 int main() {
-    srand(time(0));  //set the seed
-    int randNum = rand();
-    cout << "A random number is " << randNum << "\n";
+    //swap function
+    int a = 3;
+    int b = 5;
+    swap(a, b);
+    cout << "a is " << a << "\n";
+    cout << "b is " << b << "\n";
+    return 0;
 }
